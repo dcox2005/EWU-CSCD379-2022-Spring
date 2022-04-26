304517cfb6a4c3b8a526a4ac33f6c5e8c0811469
Harjasleen Gill <hgill3@eagles.ewu.edu>
2022-04-26 16:21:55 -0700

    revert color change

--- wordle-web/nuxt.config.js
+++ wordle-web/nuxt.config.js
@@ -63,9 +63,9 @@ export default {
           accent: colors.grey.darken3,
           secondary: colors.amber.darken3,
           info: colors.teal.lighten1,
-          warning: colors.yellow.darken2,
-          error: colors.grey.darken3,
-          success: colors.green.darken1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3,
         },
         light: {
           primary: colors.blue.darken2,
