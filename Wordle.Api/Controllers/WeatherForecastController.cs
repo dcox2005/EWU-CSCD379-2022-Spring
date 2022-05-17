37d41b8ca632c187595e0e68c3413daee4be486b
dcox2005 <65566081+dcox2005@users.noreply.github.com>
2022-05-17 14:51:40 -0700

    fixed weird issues

--- Wordle.Api/Controllers/WeatherForecastController.cs
+++ Wordle.Api/Controllers/WeatherForecastController.cs
@@ -1,41 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-namespace Wordle.api.Controllers;
-[ApiController]
-[Route("[controller]")]
-public class WeatherForecastController : ControllerBase
-{
-    private static readonly string[] Summaries = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
-    private readonly ILogger<WeatherForecastController> _logger;
-
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
-    {
-        _logger = logger;
-    }
-
-    [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
-    {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateTime.Now.AddDays(index),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
-    }
-}
-=======
-namespace Wordle.api.Controllers
-=======
 namespace Wordle.Api.Controllers
->>>>>>> remotes/Intellitect-Spring/main
 {
     [ApiController]
     [Route("Weather")]
@@ -66,4 +31,3 @@ namespace Wordle.Api.Controllers
         }
     }
 }
->>>>>>> 6aeb3a19b05d7c718c0c0cac35382d6e5af6b464
