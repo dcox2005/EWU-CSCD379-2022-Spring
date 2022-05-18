eb85e06aa53d2643816753d13f8efd827ab21233
Harjasleen Gill <hgill3@eagles.ewu.edu>
2022-05-18 16:36:50 -0700

    instructions page cont.

--- wordle-web/pages/instructions.vue
+++ wordle-web/pages/instructions.vue
@@ -1,3 +1,27 @@
 <template>
-    
+  <v-container>
+    <v-row justify="center">
+      <!-- Back Button -->
+      <v-col cols="1">
+        <v-btn
+          fab
+          v-bind="attrs"
+          color="primary"
+          v-on="on"
+          @click="$router.go(-1)"
+        >
+          <v-icon>mdi-arrow-left</v-icon>
+        </v-btn>
+      </v-col>
+
+      <!-- Game Info Card(s) -->
+      <v-col cols="12">
+        <v-card flat color="transparent">
+          <v-card-title class="justify-center">
+            <h1>How to Play</h1>
+          </v-card-title>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
