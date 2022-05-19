1c93a04cd9b470d6b07d16cf9dd605fdb58457e5
Harjasleen Gill <hgill3@eagles.ewu.edu>
2022-05-18 17:15:21 -0700

    basic instructions page made

--- wordle-web/pages/instructions.vue
+++ wordle-web/pages/instructions.vue
@@ -14,12 +14,33 @@
         </v-btn>
       </v-col>
 
-      <!-- Game Info Card(s) -->
       <v-col cols="12">
         <v-card flat color="transparent">
           <v-card-title class="justify-center">
             <h1>How to Play</h1>
           </v-card-title>
+        </v-card>
+      </v-col>
+
+      <v-col cols="6">
+        <v-card>
+          <div class="d-flex align-center">
+            <v-card-text decoration>
+              A player has 6 tries to guess a 5 letter word
+              <v-spacer />
+              After a guess if the letter is  not in the right box but is still part of the word it'll be highlighted yellow
+              <v-img src="/yellowGuess.JPG" />
+              <v-spacer />
+              <v-spacer />
+              And if the letter is in the right box it'll be highlighted green
+              <v-img src="/greenGuess.JPG" />
+              <v-spacer />
+              If the letters are not part of the word they'll show up gray
+              <v-spacer />
+              If the player guesses the word within 6 tries they win
+              <v-img src="/won.JPG" />
+            </v-card-text>
+          </div>
         </v-card>
       </v-col>
     </v-row>
