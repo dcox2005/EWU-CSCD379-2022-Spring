717fac0b107e916a65a3064e306ce3984bdf5078
dcox2005 <65566081+dcox2005@users.noreply.github.com>
2022-05-22 13:24:02 -0700

    about page resizes part 1

--- wordle-web/pages/about.vue
+++ wordle-web/pages/about.vue
@@ -5,9 +5,7 @@
       <v-col cols="1">
         <v-btn
           fab
-          v-bind="attrs"
           color="primary"
-          v-on="on"
           @click="$router.go(-1)"
         >
           <v-icon>mdi-arrow-left</v-icon>
@@ -24,29 +22,35 @@
       </v-col>
 
       <!-- Game Card -->
-      <v-col cols="8">
+      <v-col cols="12" lg="6">
         <v-card>
-          <div class="d-flex align-center">
-            <v-icon size="150" class="ma-6">mdi-file-word-box</v-icon>
-            <v-card-text decoration>
-              Not Wordle (stylized as <em>!Wordle</em>), is a web development
-              course-based game <em>heavily</em> inspired by
-              <em
-                ><a href="https://en.wikipedia.org/wiki/Josh_Wardle"
-                  >Josh Wardle</a
-                >'s</em
-              >
-              viral creation
-              <a href="https://www.nytimes.com/games/wordle/index.html"
-                >Wordle</a
-              >.
-              <v-spacer />
-              Clearly, this is not the same game, but rather a unique spin on
-              the new classic written by students at
-              <a href="https://www.ewu.edu">Eastern Washington University</a>
-              learning .NET web application development.
-            </v-card-text>
-          </div>
+          <v-row>
+            <v-col cols="12" md="4" align="center">
+              <v-icon size="150" class="ma-6">mdi-file-word-box</v-icon>
+
+            </v-col>
+            <v-col cols="12" md="8" align="center">
+              <v-card-text class="text-center text-md-left" decoration>
+                Not Wordle (stylized as <em>!Wordle</em>), is a web development
+                course-based game <em>heavily</em> inspired by
+                <em
+                  ><a href="https://en.wikipedia.org/wiki/Josh_Wardle"
+                    >Josh Wardle</a
+                  >'s</em
+                >
+                viral creation
+                <a href="https://www.nytimes.com/games/wordle/index.html"
+                  >Wordle</a
+                >.
+                <v-spacer />
+                Clearly, this is not the same game, but rather a unique spin on
+                the new classic written by students at
+                <a href="https://www.ewu.edu">Eastern Washington University</a>
+                learning .NET web application development.
+              </v-card-text>
+
+            </v-col>
+          </v-row>
         </v-card>
       </v-col>
 
@@ -60,70 +64,78 @@
       </v-col>
 
       <!-- Meg -->
-      <v-col cols="8">
+      <v-col cols="12" lg="6">
         <v-card>
-          <div class="d-flex align-center">
-            <v-avatar size="150" class="ma-6">
-              <v-img src="/meg.png" />
-            </v-avatar>
-            <v-container fluid>
-              <v-card flat>
-                <v-card-title class="justify-left">
-                  Meghan Woodford
-                </v-card-title>
-                <v-card-text>
-                  Meghan is very great... just ask her!
-                  <v-spacer />
-                  <small
-                    ><em>
-                      "Create an About page that has a few sentences about this
-                      class project <br />
-                      and <strong>how great Meg is</strong> ✅"
-                    </em>
-                    — Meg</small
-                  >
-                </v-card-text>
-                <v-card-actions>
-                  <v-btn color="primary" href="https://github.com/mmwoodfo">
-                    GitHub
-                  </v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-container>
-          </div>
+          <v-row>
+            <v-col cols="12" md="4" align="center">
+                <v-avatar size="150" class="ma-6">
+                  <v-img src="/meg.png" />
+                </v-avatar>
+            </v-col>
+            <v-col cols="12" md="8" align="center">
+                <v-container fluid>
+                  <v-card flat>
+                    <v-card-title class="justify-center justify-md-start">
+                      Meghan Woodford
+                    </v-card-title>
+                    <v-card-text class="text-center text-md-left">
+                      Meghan is very great... just ask her!
+                      <v-spacer />
+                      <small
+                        ><em>
+                          "Create an About page that has a few sentences about this
+                          class project <br />
+                          and <strong>how great Meg is</strong> ✅"
+                        </em>
+                        — Meg</small
+                      >
+                    </v-card-text>
+                    <v-card-actions class="justify-center justify-md-start">
+                      <v-btn color="primary" href="https://github.com/mmwoodfo">
+                        GitHub
+                      </v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </v-container>
+            </v-col>
+          </v-row>
         </v-card>
       </v-col>
 
       <!-- Grant -->
-      <v-col cols="8">
+      <v-col cols="12" lg="6">
         <v-card>
-          <div class="d-flex align-center">
-            <v-avatar size="150" class="ma-6">
-              <v-img src="grant.jpg" />
-            </v-avatar>
-            <v-container fluid>
-              <v-card flat>
-                <v-card-title class="justify-left">
-                  Grant Erickson
-                </v-card-title>
-                <v-card-text>
-                  Meghan is very great... just ask Grant!
-                  <v-spacer />
-                  <small
-                    ><em> "It's not necessarily defined as falsy." </em> — Grant
-                  </small>
-                </v-card-text>
-                <v-card-actions>
-                  <v-btn
-                    color="primary"
-                    href="https://github.com/GrantErickson"
-                  >
-                    GitHub
-                  </v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-container>
-          </div>
+          <v-row>
+            <v-col cols="12" md="4" align="center">
+              <v-avatar size="150" class="ma-6">
+                <v-img src="grant.jpg" />
+              </v-avatar>
+            </v-col>
+            <v-col cols="12" md="8" align="center">
+              <v-container fluid>
+                <v-card flat>
+                  <v-card-title class="justify-center justify-md-start">
+                    Grant Erickson
+                  </v-card-title>
+                  <v-card-text class="text-center text-md-left">
+                    Meghan is very great... just ask Grant!
+                    <v-spacer />
+                    <small
+                      ><em> "It's not necessarily defined as falsy." </em> — Grant
+                    </small>
+                  </v-card-text>
+                  <v-card-actions class="justify-center justify-md-start">
+                    <v-btn
+                      color="primary"
+                      href="https://github.com/GrantErickson"
+                    >
+                      GitHub
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-container>
+            </v-col>
+          </v-row>
         </v-card>
       </v-col>
     </v-row>
