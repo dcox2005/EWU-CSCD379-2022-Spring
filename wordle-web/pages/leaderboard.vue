fa985e470d546ffec334af436c10ccd8db8489d8
dcox2005 <65566081+dcox2005@users.noreply.github.com>
2022-05-22 14:12:35 -0700

    part1 leaderboard complete

--- wordle-web/pages/leaderboard.vue
+++ wordle-web/pages/leaderboard.vue
@@ -1,45 +1,58 @@
 <template>
-  <v-container fluid fill-height justify-center>
-    <v-card>
-      <v-card-title class="display-3 justify-center">
-        Leader Board
-      </v-card-title>
-      <v-card-text class="text-center">
-        {{ title }}
-      </v-card-text>
-      <v-card-text>
-        <v-simple-table>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th style="text-align: center"># Games</th>
-              <th style="text-align: center">Avg. Attempts</th>
-              <th style="text-align: center">Avg. Seconds</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="(player, playerId) in players" :key="playerId">
-              <td>{{ player.name }}</td>
-              <td style="text-align: center">{{ player.gameCount }}</td>
-              <td style="text-align: center">
-                {{ player.averageAttempts.toFixed(2) }}
-              </td>
-              <td style="text-align: center">
-                {{ player.averageSecondsPerGame }}
-              </td>
-            </tr>
-          </tbody>
-        </v-simple-table>
-      </v-card-text>
+  <v-container fluid justify-center>
+    <v-row justify="center">
+      <v-col cols="12" lg="6">
+        <v-card>
+          <v-card-title class="display-1 text-sm-display-3 justify-center">
+            Leader Board
+          </v-card-title>
+          <v-card-text class="text-center">
+            {{ title }}
+          </v-card-text>
+          <v-card-text>
+            <v-simple-table>
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th style="text-align: center"># Games</th>
+                  <th style="text-align: center">Avg. Attempts</th>
+                  <th style="text-align: center">Avg. Seconds</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="(player, playerId) in players" :key="playerId">
+                  <td>{{ player.name }}</td>
+                  <td style="text-align: center">{{ player.gameCount }}</td>
+                  <td style="text-align: center">
+                    {{ player.averageAttempts.toFixed(2) }}
+                  </td>
+                  <td style="text-align: center">
+                    {{ player.averageSecondsPerGame }}
+                  </td>
+                </tr>
+              </tbody>
+            </v-simple-table>
+          </v-card-text>
+    
+          <v-card-actions>
+            <v-row>
 
-      <v-card-actions>
-        <v-btn color="primary" @click="getAllPlayers"> Get All Players </v-btn>
-        <v-spacer />
-        <v-btn color="primary" @click="getTop10Players">
-          Get Top 10 Players
-        </v-btn>
-      </v-card-actions>
-    </v-card>
+              <v-col cols="12" md="6" align="center">
+  
+                <v-btn color="primary" @click="getAllPlayers"> Get All Players </v-btn>
+              </v-col>
+              <v-col cols="12" md="6" align="center">
+                <v-btn color="primary" @click="getTop10Players">
+                  Get Top 10 Players
+                </v-btn>
+  
+              </v-col>
+            </v-row>
+          </v-card-actions>
+        </v-card>
+
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
