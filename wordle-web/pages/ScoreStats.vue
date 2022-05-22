80ba5e9a09d827f903f9dd9ef2c4184d9ca1aba2
dcox2005 <65566081+dcox2005@users.noreply.github.com>
2022-05-22 13:52:52 -0700

    part 1 for ScoreStats page

--- wordle-web/pages/ScoreStats.vue
+++ wordle-web/pages/ScoreStats.vue
@@ -1,37 +1,44 @@
 <template>
-  <v-container fluid fill-height justify-center>
-    <v-tooltip bottom>
-      <template #activator="{ on, attrs }">
-        <v-btn color="primary" nuxt to="/" fab v-bind="attrs" v-on="on">
-          <v-icon> mdi-home </v-icon>
-        </v-btn>
-      </template>
-      <span> Go Home </span>
-    </v-tooltip>
-    <v-card>
-      <v-card-title>
-        <h1 class="display-1">Score Stats</h1>
-      </v-card-title>
-      <v-card-text>
-        <v-simple-table>
-          <thead>
-            <tr>
-              <th>Score</th>
-              <th>Avg. Seconds</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="(stat, index) in stats" :key="index">
-              <td>{{ stat.score }}</td>
-              <td>{{ stat.averageSeconds }}</td>
-            </tr>
-          </tbody>
-        </v-simple-table>
-      </v-card-text>
-      <v-card-actions>
-        <v-btn color="primary" @click="refreshStats"> Refresh </v-btn>
-      </v-card-actions>
-    </v-card>
+  <v-container fluid justify-center>
+    <v-row justify="center">
+      <v-tooltip bottom>
+        <template #activator="{ on, attrs }">
+          <v-btn color="primary" nuxt to="/" fab v-bind="attrs" v-on="on">
+            <v-icon> mdi-home </v-icon>
+          </v-btn>
+        </template>
+        <span> Go Home </span>
+      </v-tooltip>
+    </v-row>
+    <v-row justify="center">
+      <v-col cols="12" lg="6">
+
+        <v-card>
+          <v-card-title>
+            <h1 class="display-1">Score Stats</h1>
+          </v-card-title>
+          <v-card-text>
+            <v-simple-table>
+              <thead>
+                <tr>
+                  <th>Score</th>
+                  <th>Avg. Seconds</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="(stat, index) in stats" :key="index">
+                  <td>{{ stat.score }}</td>
+                  <td>{{ stat.averageSeconds }}</td>
+                </tr>
+              </tbody>
+            </v-simple-table>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn color="primary" @click="refreshStats"> Refresh </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
