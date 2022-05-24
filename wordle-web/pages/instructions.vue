b8e33a4462b3460663eda5c4e771dc88e35e1468
Harjasleen Gill <hgill3@eagles.ewu.edu>
2022-05-23 20:18:24 -0700

    More work on Instructions page

--- wordle-web/pages/instructions.vue
+++ wordle-web/pages/instructions.vue
@@ -22,27 +22,71 @@
         </v-card>
       </v-col>
 
-      <v-col cols="6">
+      <v-col cols="8">
         <v-card>
           <div class="d-flex align-center">
             <v-card-text decoration>
-              A player has 6 tries to guess a 5 letter word
+              1. A player has 6 tries to guess a 5 letter word
               <v-spacer />
-              After a guess if the letter is  not in the right box but is still part of the word it'll be highlighted yellow
-              <v-img src="/yellowGuess.JPG" />
+              2. Words can have multiples of the same letter, ex: HELLO
               <v-spacer />
+              3. After a guess if the letter is not in the right box but is still part of the word it'll be highlighted yellow
+              <v-img src="/yellow.JPG" />
               <v-spacer />
-              And if the letter is in the right box it'll be highlighted green
-              <v-img src="/greenGuess.JPG" />
-              <v-spacer />
-              If the letters are not part of the word they'll show up gray
-              <v-spacer />
-              If the player guesses the word within 6 tries they win
-              <v-img src="/won.JPG" />
             </v-card-text>
           </div>
         </v-card>
+
+        <v-card>
+          <div class="d-flex align-center">
+            <v-card-text decoration>
+              4. And if the letter is in the right box it'll be highlighted green
+              <v-img src="/green.JPG" width="1200" height="700"/>
+              <v-spacer />
+              5. If the letters are not part of the word they'll show up gray
+              <v-spacer />
+            </v-card-text>
+          </div>
+        </v-card>
+
+        <v-card>
+          <div class="d-flex align-center">
+            <v-card-text decoration>
+              6. If the player guesses the word within 6 tries they win
+              <v-img src="/won.PNG" width="1500" height="500"/>
+            </v-card-text>
+          </div>
+        </v-card>
+
+        <v-card>
+          <div class="d-flex align-center">
+            <v-card-text decoration>
+              7. Player also has the option of using the ? key to narrow down the list of options
+              <v-img src="/qMark.PNG" width="1000" height="1100"/>
+            </v-card-text>
+          </div>
+        </v-card>
+
+        <v-card>
+          <div class="d-flex align-center">
+            <v-card-text decoration>
+              Clicking on the list of available words shows the player a list of words that can be used
+              <v-img src="/qMark1.PNG" width="1000"/>
+            </v-card-text>
+          </div>
+        </v-card>
+
+        <v-card>
+          <div class="d-flex align-center">
+            <v-card-text decoration>
+              8. Another way of using the ? key
+              <v-img src="/qMark2.PNG" width="1000"/>
+            </v-card-text>
+          </div>
+        </v-card>
+
       </v-col>
+
     </v-row>
   </v-container>
 </template>
